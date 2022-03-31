af930a8a96b731a09fa8e2e47053ef78bc1072fe
Patrick Müller <p.moe.mueller@gmail.com>
2022-03-31 23:33:56 +0200

    Put all buttons in button-component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,22 +1,35 @@
-import styled from 'styled-components';
-import { IoMdArrowRoundBack } from 'react-icons/io';
+import styled, { css } from 'styled-components';
 
-export default function Button({ onClick, text }) {
-  return (
-    <ButtonStyled onClick={onClick}>
-      {text}
-      <IoMdArrowRoundBack />
-    </ButtonStyled>
-  );
-}
-
-const ButtonStyled = styled.button`
+export default styled.button`
   width: 30px;
   height: 30px;
   border-radius: 10px;
-  position: absolute;
-  left: 10px;
-  top: 10px;
   background-color: #2e5bd8;
   color: white;
+
+  ${props =>
+    props.variant === 'round' &&
+    css`
+      width: 65px;
+      height: 65px;
+      border-radius: 50%;
+      background-color: #2e5bd8;
+      color: white;
+    `}
+
+  ${props =>
+    props.variant === 'submit' &&
+    css`
+      background-color: #2e5bd8;
+      color: white;
+      width: 200px;
+      height: 50px;
+      margin-left: auto;
+      margin-right: auto;
+      border-radius: 10px;
+      font-size: 1.3rem;
+    `}
 `;
+
+// import { IoMdArrowRoundBack } from 'react-icons/io';
+// <IoMdArrowRoundBack />
