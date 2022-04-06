ad3056e79cf01cb772d352e21ff85faec7602091
Patrick Müller <p.moe.mueller@gmail.com>
2022-04-06 20:22:05 +0200

    Fix statistics.spec

--- src/components/Statistics/Statistics.spec.js
+++ src/components/Statistics/Statistics.spec.js
@@ -2,11 +2,13 @@ import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import Statistics from './Statistics';
 
+const list = [{ location: 'orlando' }, { location: 'orlando' }];
+
 describe('Statistics', () => {
   it('renders a button', () => {
     render(
       <MemoryRouter>
-        <Statistics diveData=" " />
+        <Statistics diveData={list} />
       </MemoryRouter>
     );
 
